a073da8917a17889d56eec7eac23091b22351b6e
Damien Matabon <damien.matabon@gmail.com>
2017-10-27 13:01:42 -0400

    Add Doctrine Parse adapter

--- src/Mapping/MappedEventSubscriber.php
+++ src/Mapping/MappedEventSubscriber.php
@@ -262,7 +262,8 @@ abstract class MappedEventSubscriber implements EventSubscriber
     protected function getEventAdapter(EventArgs $args)
     {
         $class = get_class($args);
-        if (preg_match('@Doctrine\\\([^\\\]+)@', $class, $m) && in_array($m[1], ['ODM', 'ORM'], true)) {
+        $class = str_replace('Redking\\ParseBundle\\Event', 'Doctrine\\Parse\\Event', $class);
+        if (preg_match('@Doctrine\\\([^\\\]+)@', $class, $m) && in_array($m[1], ['ODM', 'ORM', 'Parse'], true)) {
             if (!isset($this->adapters[$m[1]])) {
                 $adapterClass = $this->getNamespace().'\\Mapping\\Event\\Adapter\\'.$m[1];
                 if (!\class_exists($adapterClass)) {
